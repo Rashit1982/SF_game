aac8aa5e84e4a77c9c65dab99e7c079a3a2ff091
Rashit1982 <120927050+Rashit1982@users.noreply.github.com>
2022-12-18 17:20:33 +0300

    Add files via upload

--- game_v2.py
+++ game_v2.py
@@ -1,46 +1,25 @@
 """Игра сама загадывает и угадывает число"""
 import numpy as np
+
 def random_predict(number:int=1) -> int:
-    """Рандомно угадываем число
-
-    Args:
-        number (int, optional): Загаданное число. Defaults to 1.
-
-    Returns:
-        int: Число попыток
-    """
     count = 0
     
+    min, max = 1, 100
+
+    number = np.random.randint(min, max)
+
+    count = 0
+
     while True:
         count += 1
-        predict_number_up_to_100 = np.random.randint(91, 101)
-        predict_number_up_to_90 = np.random.randint(76, 91)
-        predict_number_up_to_75 = np.random.randint(61, 76)
-        predict_number_up_to_60 = np.random.randint(46, 61)
-        predict_number_up_to_45 = np.random.randint(31, 46)
-        predict_number_up_to_30 = np.random.randint(16, 31)
-        predict_number_up_to_15 = np.random.randint(1, 16)
-        if number <= 15:
-            if number == predict_number_up_to_15:
-                break
-        if 16 <= number <= 30:
-            if number == predict_number_up_to_30:
-                break
-        if 31 <= number <= 45:
-            if number == predict_number_up_to_45:
-                break
-        if 46 <= number <= 60:
-            if number == predict_number_up_to_60:
-                break
-        if 61 <= number <= 75:
-            if number == predict_number_up_to_75:
-                break
-        if 76 <= number <= 90:
-            if number == predict_number_up_to_90:
-                break
-        if number >= 91:
-            if number == predict_number_up_to_100:
-                break
+        mid = (min + max) // 2
+        
+        if mid > number:
+            max = mid
+        elif mid < number:
+            min = mid
+        else:
+            break
     return(count)
 
 print(f'Количество попыток: {random_predict()}')
